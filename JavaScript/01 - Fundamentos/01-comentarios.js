fcfce088b6e643187a2b66eb552bd0620ee7bee2
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 23:06:53 -0300

    comentarios

---
+++ JavaScript/01 - Fundamentos/01-comentarios.js
@@ -0,0 +1,19 @@
+// comentario de uma linha
+console.log('linha 01')
+/**
+    ccomentario de 
+    multiplas linhas
+*/
+ console.log('linha 02')
+/**
+ * ccomentario de 
+ * multiplas linhas
+ */
+ console.log('linha 03')
+
+/**
+ * DICAS:
+ * 01 - evitar comentarios obivios
+ * 02 - coloque nomes claros no codigo
+ * 03 - comente tudo que acreditar ser necessario!
+*/

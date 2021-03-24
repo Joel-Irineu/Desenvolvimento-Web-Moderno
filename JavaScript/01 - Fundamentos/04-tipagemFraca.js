f3e7e78643996fe733402a9a821f6d29b68425bf
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 23:19:03 -0300

    tipagem fraca

---
+++ JavaScript/01 - Fundamentos/04-tipagemFraca.js
@@ -0,0 +1,10 @@
+let qualquer = 'legal'
+console.log(qualquer, typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer, typeof qualquer)
+
+// Evitar nomes genericos e siglas para nomear variaveis
+let valor = '?'
+let numero = 1
+let pqp = false //Produto Quimico Perigoso... ksks

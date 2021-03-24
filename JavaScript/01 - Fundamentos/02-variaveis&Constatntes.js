ccfa5ea3939e45fe32c7910845bb631984232fca
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 23:12:55 -0300

    variaveis e constantes

---
+++ JavaScript/01 - Fundamentos/02-variaveis&Constatntes.js
@@ -0,0 +1,16 @@
+// os nomes de variaveis são importantes, procure nomes que façam sentindos!
+var a = 3
+let b = 4 // dar prioridade ao let
+console.log(a, b)
+
+var a = 30
+b = 40
+console.log(a, b)
+
+a = 300
+b = 400
+console.log(a, b)
+
+const c = 5
+// c = 50 -> retorna erro, pois const não pode ser reatribuido
+console.log(c)
